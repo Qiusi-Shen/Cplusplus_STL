18da0b083566a27059231b38f76d4eeb52d23138
Qiusi-Shen <qiusi@ucla.edu>
2018-05-03 17:02:33 -0700

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,4 +1,11 @@
+// Introduction to templates
+
 template <typename T>
 T square(T x) {
     return x*x;
+}
+
+int main(){        // Code bloat
+    cout << square(5) << endl;
+    cout << square(5.5) << endl;
 }
