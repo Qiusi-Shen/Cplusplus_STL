430528753d50f79521dcfc2e2d307e3aca6eb864
Qiusi-Shen <qiusi@ucla.edu>
2018-05-03 17:09:26 -0700

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -5,6 +5,21 @@ T square(T x) {
     return x*x;
 }
 
+template <typename T>
+class BoVector {
+    T arr[1000];
+    int size;
+public:
+    BoVector():size(0){}
+    void push(T x) {arr[size] = x; size++;}
+    T get(int i) const{return arr[i];}
+    int getSize() const { return size;}
+    void print() const {for(int i =0; i < size; i++) {cout<<arr[i]<<endl;}}
+};
+
+template <typename T>
+BoVector<T>
+
 int main(){        // Code bloat
     cout << square(5) << endl;
     cout << square(5.5) << endl;
